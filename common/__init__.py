e8fc174210c7255c08fdd81849b404061f52116c
ToolsWatch <hacker@toolswatch.org>
2020-12-23 21:07:52 +0100

    update version

--- common/__init__.py
+++ common/__init__.py
@@ -1,6 +1,7 @@
-# API Python wrapper for The Next Generation Vulnerability & Threat Intelligence Database  - https://vfeed.io
-# Copyright (C) 2013 - 2019 vFeed IO
+#!/usr/bin/env python3
+# API Python wrapper for The Vulnerability & Threat Intelligence Feed Service
+# Copyright (C) 2013 - 2020 vFeed, Inc. - https://vfeed.io
 
 __title__ = "Python 3.x API For vFeed Professional Edition"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __email__ = "support@vfeed.io"
